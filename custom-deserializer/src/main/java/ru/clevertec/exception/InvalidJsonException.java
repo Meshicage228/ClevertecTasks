e0b550373c4d5e875015ecbfde1e4b2c81c744cd
Vlad.Zherko <vladikon19@tut.by>
2024-09-11 15:21:50 +0300

    deleted

--- custom-deserializer/src/main/java/ru/clevertec/exception/InvalidJsonException.java
+++
@@ -1,9 +0,0 @@
-package ru.clevertec.exception;
-
-public class InvalidJsonException extends AbstractException{
-    private final static String message = "Invalid JSON!";
-
-    public InvalidJsonException() {
-        super(message);
-    }
-}
