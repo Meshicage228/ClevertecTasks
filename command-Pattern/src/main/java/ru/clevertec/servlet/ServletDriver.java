1503bb9263fcc4b6f54d8344edc27e5a58eb2579
Vlad.Zherko <vladikon19@tut.by>
2024-09-21 00:56:50 +0300

    improved driver

--- command-Pattern/src/main/java/ru/clevertec/servlet/ServletDriver.java
+++ command-Pattern/src/main/java/ru/clevertec/servlet/ServletDriver.java
@@ -14,18 +14,34 @@ public class ServletDriver extends HttpServlet {
     private CommandInvoker commandInvoker;
 
     @Override
-    public void init() throws ServletException {
+    public void init() {
         commandInvoker = new CommandInvoker();
     }
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String command = req.getParameter("command");
+    protected void service(HttpServletRequest request, HttpServletResponse response) {
+        String command = request.getParameter("command");
 
-        if(command != null){
-            commandInvoker.invoke(command);
+        if (command != null) {
+            commandInvoker.getCommand(command)
+                    .ifPresentOrElse(
+                            service -> processServiceResponse(service.execute(request, response), request, response),
+                            () -> response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR)
+                    );
         }
+    }
 
-        super.doGet(req, resp);
+    private void processServiceResponse(String executed, HttpServletRequest request, HttpServletResponse response) {
+        try {
+            if (executed.equals("success")) {
+                response.sendRedirect("success.jsp");
+            } else if (executed.equals("failure")) {
+                request.getRequestDispatcher("failure.jsp").forward(request, response);
+            } else {
+                response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            }
+        } catch (IOException | ServletException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
